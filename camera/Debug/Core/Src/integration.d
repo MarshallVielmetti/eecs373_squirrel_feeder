90086f7d244296a87a0f908a0c8de648c9542027
Vielmetti <mvielmet@UMICH.EDU>
2024-12-05 09:52:17 -0500

    removed camera dir

--- camera/Debug/Core/Src/integration.d
+++
@@ -1,7 +0,0 @@
-Core/Src/integration.o: ../Core/Src/integration.c ../Core/Inc/motor.h \
- ../Core/Inc/load_cell.h ../Core/Inc/ir_sensor.h \
- ../Core/Inc/camera_handler.h
-../Core/Inc/motor.h:
-../Core/Inc/load_cell.h:
-../Core/Inc/ir_sensor.h:
-../Core/Inc/camera_handler.h:
--- camera/Debug/Drivers/LCD/Src/fonts.d
+++
@@ -1,3 +0,0 @@
-Drivers/LCD/Src/fonts.o: ../Drivers/LCD/Src/fonts.c \
- C:/Users/nehakan/Downloads/camera/Drivers/LCD/Inc/fonts.h
-C:/Users/nehakan/Downloads/camera/Drivers/LCD/Inc/fonts.h:
